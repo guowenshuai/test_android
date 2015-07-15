c90295166333ae071c57923c5323759395ae0605
guowenshuai <guowenshuai8207@163.com>
2015-07-15 08:04:54 +0800

    new activity

---
+++ src/com/example/test/Report.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gws on 15-7-15.
+ */
+public class Report extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rreport);
+    }
+}
